6cea525de5e411c41d070fd8a2dd335cb321b9f0
katthigokul <gokulkatthi@gmail.com>
2019-07-12 12:50:27 +0530

    Created Test Cases for VowelCheck

---
+++ src/test/java/com/stackroute/pe1/VowelCheckTest.java
@@ -0,0 +1,24 @@
+package com.stackroute.pe1;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VowelCheckTest {
+    VowelCheck vowelCheck;
+
+    @Before
+    public void setUp() throws Exception {
+        vowelCheck = new VowelCheck();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        vowelCheck = null;
+    }
+
+    @Test
+    public void checkCharacter() {
+
+    }
+}
