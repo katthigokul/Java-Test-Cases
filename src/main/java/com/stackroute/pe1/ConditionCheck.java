7a3df7bec8faf6fb16761cbf45e2e69dda02cc7d
K GOKUL <gokulkatthi@gmail.com>
2019-07-16 10:40:10 +0530

    Update ConditionCheck.java

--- src/main/java/com/stackroute/pe1/ConditionCheck.java
+++ src/main/java/com/stackroute/pe1/ConditionCheck.java
@@ -1,4 +1,11 @@
 package com.stackroute.pe1;
+/**
+ * Practice Exercise Question - 2
+ * Class accepts an integer number as input from the user and perform the
+ * following conditional checks:
+ * a. Print Tom if number is odd and exists between 20 to 30
+ * b. Print Jerry, if number is even and exists between 20 and 30
+ */
 
 public class ConditionCheck {
     public String  conditionCheck (int n){
