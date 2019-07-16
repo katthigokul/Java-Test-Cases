0285a912b43a4a580f09cd545b57f4ea85e3b790
K GOKUL <gokulkatthi@gmail.com>
2019-07-16 10:54:20 +0530

    Update VowelCheck.java

--- src/main/java/com/stackroute/pe1/VowelCheck.java
+++ src/main/java/com/stackroute/pe1/VowelCheck.java
@@ -1,25 +1,29 @@
 package com.stackroute.pe1;
+/**
+ * Practice Exercise Question - 3
+ * Class that accepts a word as input and checks for each single character letter in
+ * the word whether it is a consonant or vowel.
+ * Condition:
+ * a. Print an error message if the input is not a letter
+ * b. If the input having more than one letter, print the required output
+ * (Vowel or Consonant) for each letter
+ */
 
 import java.util.Scanner;
 public class VowelCheck {
-    public static void main(String[] args) {
-            Scanner s = new Scanner(System.in);
-            System.out.print("Enter a word: ");
-            String input = s.next();
-            if ((input != null) && (!input.isEmpty()) && (!input.matches(".*\\d.*"))) {
-                char[] inputArray = input.toCharArray();
-                for (char c : inputArray) {
-                    if (Character.toLowerCase(c) == 'a' || Character.toLowerCase(c) == 'e' || Character.toLowerCase(c) == 'i' || Character.toLowerCase(c) == 'o' || Character.toLowerCase(c) == 'u') {
-                        System.out.println(c + " - Vowel");
-                    } else {
-                        System.out.println(c + " -Consonant");
-                    }
+       public String checkCharacter(String input) {
+        if ((input != null) && (!input.isEmpty()) && (!input.matches(".*\\d.*"))) {
+            char[] inputArray = input.toCharArray();
+            for (char c : inputArray) {
+                if (Character.toLowerCase(c) == 'a' || Character.toLowerCase(c) == 'e' || Character.toLowerCase(c) == 'i' || Character.toLowerCase(c) == 'o' || Character.toLowerCase(c) == 'u') {
+                    return (c + " - Vowel");
+                } else {
+                    System.out.println(c + " - Consonant");
                 }
-            } else {
-                System.out.println("Please give a valid string");
             }
-            /*Close the scanner*/
-            s.close();
-
+        } else {
+            return ("Please give a valid string");
+        }
+        return input;
     }
 }
