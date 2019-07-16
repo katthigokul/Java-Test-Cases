977338800de5badedd77180a46566a803db4db30
K GOKUL <gokulkatthi@gmail.com>
2019-07-16 10:59:24 +0530

    Update Guess.java

--- src/main/java/com/stackroute/pe1/Guess.java
+++ src/main/java/com/stackroute/pe1/Guess.java
@@ -1,5 +1,12 @@
 package com.stackroute.pe1;
-
+/* Practice 8
+Write a program which accepts a number from user as input (set the limit say 1 - 50 or 1 -
+100) User should guess until the the target number is guessed correctly.
+Print separate messages for the following:
+a. Number guessed is more than original number
+b. Number guessed is less than original number
+c. Number guessed matches the original number
+*/
 
 import java.util.Random;
 import java.util.Scanner;
